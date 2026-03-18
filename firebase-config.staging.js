3d245d78e95a7fbbed1c4894745f51982392c763
Dominic <1772131+domdip@users.noreply.github.com>
2026-03-17 17:52:35 -0700

    Fix staging Firebase config formatting

--- firebase-config.staging.js
+++ firebase-config.staging.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAarIMkq1p7VgkyX6iaQhVU41xqXo_qJ2s",
-  authDomain: "AIzaSyAarIMkq1p7VgkyX6iaQhVU41xqXo_qJ2s",
+  authDomain: "separation-anxiety-staging.firebaseapp.com",
   projectId: "separation-anxiety-staging",
-  storageBucket: "separation-anxiety-staging",
+  storageBucket: "separation-anxiety-staging.firebasestorage.app",
   messagingSenderId: "753282727661",
   appId: "1:753282727661:web:a3128448b2bfbbf3e496e4",
 };
